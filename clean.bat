d412d2ba19d8fe3a731b77c9e720356ffa98a854
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-02-26 22:17:57 +0700

    Change clean.bat to delete obj and bin folder in Testing module

--- clean.bat
+++ clean.bat
@@ -5,5 +5,7 @@ pause
 rmdir /q /s  .\SocialNetworkBE\obj
 rmdir /q /s  .\SocialNetworkBE\bin
 echo obj, bin in SocialNetworkBE folder clear successfully!
-
+rmdir /q /s  .\Testing\obj
+rmdir /q /s  .\Testing\bin
+echo obj, bin in SocialNetworkBE folder clear successfully!
 pause
