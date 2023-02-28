90c264ca4172d968513093e0084763505258f6d7
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-02-28 15:41:54 +0700

    Change Method name of RedisCacheService

--- SocialNetworkBE/Services/RedisCache/RedisCacheService.cs
+++ SocialNetworkBE/Services/RedisCache/RedisCacheService.cs
@@ -8,19 +8,11 @@ using System.Web;
 namespace SocialNetworkBE.Services.RedisCache {
     public class RedisCacheService {
         private readonly string Host = "localhost";
-        private readonly Int32 Port = 8080;
+        private readonly int Port = 8080;
         private readonly RedisEndpoint redisEndpoint;
 
         public RedisCacheService() {
-            redisEndpoint = GetRedisEndpoint();
-        }
-        public RedisEndpoint GetRedisEndpoint() {
-            return new RedisEndpoint(Host, Port);
-        }
-
-        public void SetObjectToCache(string key, string value) {
-            var regisClient = new RedisClient(redisEndpoint);
-            regisClient.SetValue(key, value);
+            redisEndpoint = new RedisEndpoint(Host, Port);
         }
 
         public void SetObjectToCache(string key, object objectValue) {
@@ -28,13 +20,26 @@ namespace SocialNetworkBE.Services.RedisCache {
             string jsonObjectData = JsonSerializer.Serialize<object>(objectValue);
             regisClient.SetValue(key, jsonObjectData);
         }
-
-        public string GetObjectFromCache(string key) {
-            var regisClient = new RedisClient(redisEndpoint);
-            return regisClient.GetValue(key);
+        public bool IsExistsKey(string key) {
+            using (var redisClient = new RedisClient(redisEndpoint)) {
+                if (redisClient.ContainsKey(key)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        // JsonSerializer.Deserialize<Object>(jsonString); To get object from cache
+        public string GetJsonObjectFromCache(string key) {
+            if (IsExistsKey(key)) {
+                var regisClient = new RedisClient(redisEndpoint);
+                return regisClient.GetValue(key);
+            }
+            return "";
         }
 
-        public bool StoreList<T>(string key, T value, TimeSpan timeout) {
+        public bool SetListObjectToCache<T>(string key, T value, TimeSpan timeout) {
             try {
                 using (var redisClient = new RedisClient(redisEndpoint)) {
                     redisClient.As<T>().SetValue(key, value, timeout);
@@ -55,16 +60,6 @@ namespace SocialNetworkBE.Services.RedisCache {
             }
 
             return result;
-        }
-        public bool IsExistsKey(string key) {
-            using (var redisClient = new RedisClient(redisEndpoint)) {
-                if (redisClient.ContainsKey(key)) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
         }
     }
 }
