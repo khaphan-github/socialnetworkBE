c9d22f19a60181fc2bb93d2d168b6a1f903b3d7f
kimkhanhneee0201hihi <77334607+kimkhanhneee0201hihi@users.noreply.github.com>
2023-03-01 13:33:09 +0700

    get posts

---
+++ SocialNetworkBE/Repositorys/DataModels/Like.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace SocialNetworkBE.Repositorys.DataModels
+{
+    public class Like
+    {
+        public ObjectId Id { set; get; }
+        public string TypeofHeart { set; get; }
+        public DateTime CreateDate { set; get; }
+    }
+}
