bc76a4f01d24de0386fd3417f264fc500c332b4b
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-03-04 00:08:33 +0700

    Delte v17 test log file in .vs/SocialNetoworkBE

--- clean.bat
+++ clean.bat
@@ -5,7 +5,11 @@ pause
 rmdir /q /s  .\SocialNetworkBE\obj
 rmdir /q /s  .\SocialNetworkBE\bin
 echo obj, bin in SocialNetworkBE folder clear successfully!
+
 rmdir /q /s  .\Testing\obj
 rmdir /q /s  .\Testing\bin
 echo obj, bin in Testing folder clear successfully!
+
+rmdir /q /s .\.vs\SocialNetworkBE\v17\
+echo .vs/SocialNetworkBE folder clear successfully!
 pause
