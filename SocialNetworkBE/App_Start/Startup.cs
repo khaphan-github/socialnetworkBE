31052850605c317f13fa17e837cfe6592bc8c615
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-03-25 14:44:35 +0700

    Config signalR server

---
+++ SocialNetworkBE/App_Start/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialNetworkBE.App_Start.Startup))]
+namespace SocialNetworkBE.App_Start {
+    public class Startup {
+        const string SignalRServerURL = "/notification-server";
+        const string SignalRChatServerURL = "/notification-chanel";
+
+        public void Configuration(IAppBuilder app) {
+
+            // Any connection or hub wire up and configuration should go here
+            app.Map(SignalRServerURL, map => {
+                var hubConfiguration = new HubConfiguration {
+
+                };
+                map.RunSignalR(hubConfiguration);
+            });
+
+            app.Map(SignalRChatServerURL, map => {
+                var hubConfiguration = new HubConfiguration {
+
+                };
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
--- SocialNetworkBE/Services/SignalR/SignalRService.cs
+++ SocialNetworkBE/Services/SignalR/SignalRService.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.AspNet.SignalR;
+using System;
 
 namespace SocialNetworkBE.Services.SignalR {
-    public class SignalRService {
-
+    public class SignalRService :Hub {
+        public void Ping() {
+            Clients.Client(Context.ConnectionId).Pong("SIGNALR  Connection Establish!");
+        }
+        private void Log(string message) {
+            string time = DateTime.Now.ToString();
+            System.Diagnostics.Debug.WriteLine(time + ": " + message);
+        }
     }
 }
