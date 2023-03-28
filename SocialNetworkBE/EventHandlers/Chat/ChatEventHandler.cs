d67cbb6b2e45900f09c70513153efa3fc64a88cc
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-03-28 14:19:08 +0700

    Finish get document stored chat in Firebase

---
+++ SocialNetworkBE/EventHandlers/Chat/ChatEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using SocialNetworkBE.Payload.Response;
+using SocialNetworkBE.Repositorys;
+using SocialNetworkBE.Repositorys.DataModels;
+using System.Collections.Generic;
+
+namespace SocialNetworkBE.EventHandlers.Chat {
+    public class ChatEventHandler {
+        private readonly RoomChatRepository RoomChatRepository = new RoomChatRepository();
+
+        public ResponseBase GetRoomChat(ObjectId firstParicipate, ObjectId secondParticipate) {
+            List<ObjectId> paricipateId = new List<ObjectId>() { firstParicipate, secondParticipate };
+            RoomChat roomChat = RoomChatRepository.GetRoomChatByPaticipate(paricipateId);
+
+            if (roomChat == null) {
+                roomChat = new RoomChat(paricipateId);
+                RoomChatRepository.CreateRoomChat(roomChat);
+            }
+
+            return new ResponseBase() {
+                Status = Status.Success,
+                Message = "Get room chat success",
+                Data = roomChat
+            };
+
+        }
+    }
+}
