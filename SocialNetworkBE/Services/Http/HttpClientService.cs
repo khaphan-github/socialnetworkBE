ec1148baea5bf97df24f23e0f9e4368e21615187
kimkhanhneee0201hihi <77334607+kimkhanhneee0201hihi@users.noreply.github.com>
2023-03-29 21:13:56 +0700

    Push lợi hong mất code

---
+++ SocialNetworkBE/Services/Http/HttpClientService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkBE.Services.Notification
+{
+    public class HttpClientService
+    {
+        private readonly HttpClient httpClient;
+        private readonly string contentType = "application/json";
+        public HttpClientService()
+        {
+            httpClient = new HttpClient();
+        }
+        public async Task<HttpResponseMessage> PostAsync(string url, string content)
+        {
+            var httpContent = new StringContent(content, Encoding.UTF8, contentType);
+            return await httpClient.PostAsync(url, httpContent);
+        }
+
+        public async Task<HttpResponseMessage> PutAsync(string url, string content)
+        {
+            var httpContent = new StringContent(content, Encoding.UTF8, contentType);
+            return await httpClient.PutAsync(url, httpContent);
+        }
+    }
+}
