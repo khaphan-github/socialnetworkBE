2648833121aa94500a1b43fda7605349996be091
kimkhanhneee0201hihi <77334607+kimkhanhneee0201hihi@users.noreply.github.com>
2023-03-30 13:17:00 +0700

    add userPost

--- SocialNetworkBE/EventHandlers/PostHandler/PostEventHandler.cs
+++ SocialNetworkBE/EventHandlers/PostHandler/PostEventHandler.cs
@@ -71,6 +71,7 @@ namespace SocialNetworkBE.EventHandlers.PostHandler {
             // TODO: Get like post
             bool isRightCommentId = ObjectId.TryParse(userMetadata.Id, out var userId);
             List<ObjectId> listFriend = AccountRepostitory.ListFriendByUserId(userId);
+            listFriend.Add( ObjectId.Parse(userMetadata.Id));
             List<PostDataTranfer> AllFriendPost = new List<PostDataTranfer>();
             foreach(ObjectId i in listFriend)
             {
