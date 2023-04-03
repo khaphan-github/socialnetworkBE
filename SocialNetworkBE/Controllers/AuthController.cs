f1a58a0ea256f0b1c98358354427992aca7cd7d2
kimkhanhneee0201hihi <77334607+kimkhanhneee0201hihi@users.noreply.github.com>
2023-04-04 00:22:11 +0700

    fix bug at refresh token

--- SocialNetworkBE/Controllers/AuthController.cs
+++ SocialNetworkBE/Controllers/AuthController.cs
@@ -1,11 +1,19 @@
-﻿using ServiceStack;
+﻿using Microsoft.IdentityModel.Tokens;
+using ServiceStack;
 using SocialNetworkBE.Payload.Request;
 using SocialNetworkBE.Payload.Response;
+using SocialNetworkBE.Payloads.Data;
 using SocialNetworkBE.Payloads.Request;
 using SocialNetworkBE.ServerConfiguration;
 using SocialNetworkBE.Services.Authenticate;
+using SocialNetworkBE.Services.JsonWebToken;
 using SocialNetworkBE.Services.Notification;
+using System;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using RouteAttribute = System.Web.Http.RouteAttribute;
@@ -16,6 +24,12 @@ namespace SocialNetworkBE.Controllers {
         private const string REFIX = "api/v1/auth";
         private readonly AuthService authService = new AuthService();
         private PushNotificationService _pushNotificationService;
+        private readonly JsonWebTokenService jsonWebTokenService;
+
+        public AuthController()
+        {
+            jsonWebTokenService = new JsonWebTokenService();
+        }
 
         [HttpPost]
         [Route(REFIX + "/")]
@@ -99,21 +113,58 @@ namespace SocialNetworkBE.Controllers {
 
         [HttpPost]
         [Route(REFIX + "/token")]
-        public async Task<ResponseBase> RefreshToken([FromBody] Token tokenRequest) {
-            bool isEmptyParams = 
-                string.IsNullOrWhiteSpace(tokenRequest.AccessToken) || 
+        public async Task<ResponseBase> RefreshToken([FromBody] Token tokenRequest)
+        {
+            bool isEmptyParams =
+                string.IsNullOrWhiteSpace(tokenRequest.AccessToken) ||
                 string.IsNullOrWhiteSpace(tokenRequest.RefreshToken);
-            
-            if (isEmptyParams) {
-                return new ResponseBase() {
+
+            if (isEmptyParams)
+            {
+                return new ResponseBase()
+                {
                     Message = "Request missing AccessToken or RefreshToken in request's body",
                     Status = Status.WrongFormat,
                 };
             }
-            
+            var tokenResponse = authService.HandleRefreshTokenAsync(tokenRequest);
+            ClaimsIdentity validAccessToken = jsonWebTokenService.GetClaimsIdentityFromToken(tokenRequest.AccessToken);
+            if (validAccessToken == null)
+            {
+                return new ResponseBase()
+                {
+                    Status = Status.Unauthorized,
+                    Message = "Access token is invalid or expired."
+                };
+            }
 
-            return authService.HandleRefreshTokenAsync(tokenRequest);
+            // Lấy access token mới từ đối tượng TokenResponse
+            bool isAccessTokenType = jsonWebTokenService
+            .GetValueFromClaimIdentityByTypeClaim(validAccessToken, jsonWebTokenService.KeyClaimsToken)
+            .Equals(jsonWebTokenService.AccessToken);
+            if (!isAccessTokenType)
+            {
+                return new ResponseBase()
+                {
+                    Status = Status.Unauthorized,
+                    Message = "Token in AccessToken param is not an access token"
+                };
+            }
+
+            // Get user metadata
+            string refreshToken = tokenRequest.RefreshToken;
+            UserMetadata userMetadata = new UserMetadata().GetUserMetadataFromToken(refreshToken);
+            var userId = userMetadata.Id;
+            Debug.WriteLine(userId);
+            
+            _pushNotificationService = new PushNotificationService(userId, tokenRequest.AccessToken);
+            await _pushNotificationService.SendAccessTokenToNotifiService();
+            
+            // Gọi phương thức HandleRefreshTokenAsync để làm mới access token và trả về response chứa token mới
+            
+            return tokenResponse;
         }
-        
+
+
     }
 }
--- SocialNetworkBE/Payloads/Request/UserMetadata.cs
+++ SocialNetworkBE/Payloads/Request/UserMetadata.cs
@@ -1,8 +1,12 @@
 ﻿using SocialNetworkBE.ServerConfiguration;
+using SocialNetworkBE.Services.JsonWebToken;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Net.Http;
 
 namespace SocialNetworkBE.Payloads.Request {
     public class UserMetadata {
+        private JsonWebTokenService jsonWebTokenService;
         public string Id { get; set; }
         public string AvatarURL { get; set; }
         public string DisplayName { set; get; }
@@ -14,6 +18,13 @@ namespace SocialNetworkBE.Payloads.Request {
                     out var outUserMetadata
                 );
             return outUserMetadata as UserMetadata;
+        }
+        public UserMetadata GetUserMetadataFromToken(string refreshToken)
+        {
+            var claimsIdentity = jsonWebTokenService.GetClaimsIdentityFromToken(refreshToken);
+            var userId = jsonWebTokenService.GetValueFromClaimIdentityByTypeClaim(claimsIdentity, "UserId");
+            var role = jsonWebTokenService.GetValueFromClaimIdentityByTypeClaim(claimsIdentity, "Role");
+            return new UserMetadata() { Id = userId};
         }
     }
 }
